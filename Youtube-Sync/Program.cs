3ef67dbfe382cf65c048b2015a94b23110186f2e
jakubsuchybio <dev@jakubsuchy.com>
2019-08-03 14:25:13 +0200

    Finishes AB#44 - Add TopShelf as windows service handler - Add Serilog for logging everything including topshelf stuff

--- Youtube-Sync/Program.cs
+++ Youtube-Sync/Program.cs
@@ -1,12 +1,63 @@
 ﻿using System;
+using System.Timers;
+using Serilog;
+using Topshelf;
 
 namespace Youtube_Sync
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .WriteTo.File("log.txt")
+                .CreateLogger();
 
+            HostFactory.Run(windowsService =>
+            {
+                windowsService.Service<YoutubeSyncService>(s =>
+                {
+                    s.ConstructUsing(service => new YoutubeSyncService());
+                    s.WhenStarted(service => service.Start());
+                    s.WhenStopped(service => service.Stop());
+                });
+
+                windowsService.UseSerilog();
+                windowsService.RunAsLocalSystem();
+                windowsService.StartAutomatically();
+
+                windowsService.SetDescription("This service synchronizes youtube play-lists with filesystem");
+                windowsService.SetDisplayName("rEv-soft Youtube Sync");
+                windowsService.SetServiceName("rEv-soft Youtube Sync");
+            });
+        }
+
+        private class YoutubeSyncService
+        {
+            private readonly Timer _timer;
+
+            public YoutubeSyncService()
+            {
+
+                _timer = new Timer { AutoReset = true, Interval = 30000 };
+                _timer.Elapsed += Timer_Elapsed;
+            }
+
+            private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+            {
+                
+            }
+
+            public void Start()
+            {
+                _timer.Start();
+            }
+
+            public void Stop()
+            {
+                _timer.Stop();
+            }
         }
     }
 }
