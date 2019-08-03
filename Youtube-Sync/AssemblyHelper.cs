fb2c84241dfe10a71cfa7791ec3348022d519639
jakubsuchybio <dev@jakubsuchy.com>
2019-08-03 16:28:32 +0200

    Few refactorings

---
+++ Youtube-Sync/AssemblyHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace Youtube_Sync
+{
+    internal class AssemblyHelper
+    {        
+        public static string AssemblyTitle
+        {
+            get
+            {
+                var customAttributes = Assembly.GetCallingAssembly().GetCustomAttributes(typeof(AssemblyTitleAttribute), false);
+                if (customAttributes.Length != 0)
+                {
+                    var assemblyTitleAttribute = (AssemblyTitleAttribute)customAttributes[0];
+                    if (assemblyTitleAttribute.Title != "")
+                        return assemblyTitleAttribute.Title;
+                }
+
+                return Path.GetFileNameWithoutExtension(Assembly.GetCallingAssembly().CodeBase);
+            }
+        }
+        public static string AssemblyVersion => Assembly.GetCallingAssembly().GetName().Version.ToString();
+        public static Assembly CurrentAssembly => Assembly.GetCallingAssembly();
+    }
+}
--- Youtube-Sync/Program.cs
+++ Youtube-Sync/Program.cs
@@ -6,31 +6,39 @@ namespace Youtube_Sync
 {
     public class Program
     {
-        public static void Main(string[] args)
+        private const string UPDATE_DIR = @"c:\Updates\Youtube-Sync\";
+
+        public static void Main()
         {
             Log.Logger = new LoggerConfiguration()
                 .WriteTo.Console()
                 .WriteTo.File("log.txt")
                 .CreateLogger();
+            
+            Log.Information("##########   Starting process '{0}', V '{1}'   ##########",
+                AssemblyHelper.AssemblyTitle,
+                AssemblyHelper.AssemblyVersion);
 
             AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
-
-            HostFactory.Run(windowsService =>
+            
+            HostFactory.Run(hostConfig =>
             {
-                windowsService.Service<YoutubeSyncService>(s =>
+                hostConfig.Service<YoutubeSyncService>(s =>
                 {
-                    s.ConstructUsing(service => new YoutubeSyncService());
-                    s.WhenStarted(service => service.Start());
-                    s.WhenStopped(service => service.Stop());
+                    s.ConstructUsing(name => new YoutubeSyncService());
+                    s.WhenStarted(ys => ys.Start());
+                    s.WhenStopped(ys => ys.Stop());
                 });
+                
+                hostConfig.EnableServiceRecovery(rc => rc.RestartService(1));
+                hostConfig.EnableSessionChanged();
+                hostConfig.UseSerilog();
+                hostConfig.RunAsLocalSystem();
+                hostConfig.StartAutomatically();
 
-                windowsService.UseSerilog();
-                windowsService.RunAsLocalSystem();
-                windowsService.StartAutomatically();
-
-                windowsService.SetDescription("This service synchronizes youtube play-lists with filesystem");
-                windowsService.SetDisplayName("rEv-soft Youtube Sync");
-                windowsService.SetServiceName("rEv-soft Youtube Sync");
+                hostConfig.SetDescription("This service synchronizes youtube play-lists with filesystem");
+                hostConfig.SetDisplayName("rEv-soft Youtube Sync");
+                hostConfig.SetServiceName("rEv-soft Youtube Sync");
             });
         }
 
