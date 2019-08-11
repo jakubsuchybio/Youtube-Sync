73d3d7be4cce0587bbba497b70b1793c4419e9c7
jakubsuchybio <dev@jakubsuchy.com>
2019-08-11 13:54:20 +0200

    Add squirrel updater

--- src/Youtube-Sync/Program.cs
+++ src/Youtube-Sync/Program.cs
@@ -1,14 +1,21 @@
 ﻿using System;
-using System.ServiceProcess;
+using System.Diagnostics;
+using System.Reflection;
 using Serilog;
 using Topshelf;
+using Squirrel;
+using System.Threading.Tasks;
+using NuGet;
+
+[assembly: AssemblyMetadata("SquirrelAwareVersion", "1")]
 
 namespace Youtube_Sync
 {
     public class Program
     {
+        private const string _updateUrl = "http://jakubsuchy.com/Youtube-Sync/";
         private const string LOGGER_TEMPLATE = "{Timestamp:HH:mm:ss.fffffff zzz} [{Level:u3}] [{ThreadId}] {Message:j}{NewLine}{Exception}";
-        
+
         public static void Main()
         {
             Log.Logger = new LoggerConfiguration()
@@ -16,11 +23,13 @@ namespace Youtube_Sync
                 .WriteTo.File("C:/ProgramData/youtube-sync.txt", outputTemplate: LOGGER_TEMPLATE)
                 .CreateLogger();
 
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+
+            Task.Factory.StartNew(CheckForUpdates, TaskCreationOptions.LongRunning);
+
             Log.Information("##########  Starting process '{0}', V '{1}'  ##########",
                 AssemblyHelper.AssemblyTitle,
                 AssemblyHelper.AssemblyVersion);
-
-            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
 
             // Start TopShelf 
             HostFactory.Run(hostConfig =>
@@ -31,7 +40,7 @@ namespace Youtube_Sync
                     s.WhenStarted(ys => ys.Start());
                     s.WhenStopped(ys => ys.Stop());
                 });
-                
+
                 hostConfig.EnableServiceRecovery(rc => rc.RestartService(1));
                 hostConfig.EnableSessionChanged();
                 hostConfig.UseSerilog();
@@ -42,6 +51,56 @@ namespace Youtube_Sync
                 hostConfig.SetDisplayName("rEv-soft Youtube Sync");
                 hostConfig.SetServiceName("rEv-soft Youtube Sync");
             });
+        }
+
+        private static async Task CheckForUpdates()
+        {
+            using (var mgr = new UpdateManager(_updateUrl))
+            {
+                while (true)
+                {
+                    var currentVersion = Assembly.GetEntryAssembly().GetName().Version.ToString();
+                    try
+                    {
+                        // Check for update
+                        var update = await mgr.CheckForUpdate();
+                        try
+                        {
+                            var oldVersion = update.CurrentlyInstalledVersion?.Version ??
+                                             new SemanticVersion(0, 0, 0, 0);
+                            Log.Information("Installed version: {0}", oldVersion);
+                            var newVersion = update.FutureReleaseEntry.Version;
+                            if (oldVersion < newVersion)
+                            {
+                                Log.Information("Found a new version: {0}", newVersion);
+
+                                // Download Release
+                                await mgr.DownloadReleases(update.ReleasesToApply);
+
+                                // Apply Release
+                                await mgr.ApplyReleases(update);
+
+                                CallCustomUpdateScript();
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            Log.Fatal(ex, $"Error on update {currentVersion}");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Log.Fatal(ex, $"Error on check for update {currentVersion}");
+                    }
+
+                    await Task.Delay(5000);
+                }
+            }
+        }
+
+        private static void CallCustomUpdateScript()
+        {
+            Debugger.Launch();
         }
 
         private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
